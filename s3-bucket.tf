730a33488ebe08afc1b429c46cb4e2969980641f
Kristian Tusha <kristiant19@gmail.com>
2021-10-07 11:54:06 +0000

    Added s3 bucket module7

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,9 +2,9 @@ module "s3-bucket" {
   source  = "app.terraform.io/company3221/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket_prefix = "kristiantusha"
 
   bucket = "my-s3-bucket"
+  bucket_prefix = "kt"
   acl    = "private"
 
   versioning = {
